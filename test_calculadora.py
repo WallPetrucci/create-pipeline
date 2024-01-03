a2255b35210e4c4de3f9ff521aa1cb1e40e17e5b
Wallace Petrucci <wallacepetrucci@gmail.com>
2024-01-03 18:49:31 -0300

    removendo tests para testar o block de coverage

--- test_calculadora.py
+++ test_calculadora.py
@@ -5,17 +5,17 @@ def test_que_calculadora_soma():
     result = calc.soma(5, 2)
     assert result == 7
 
-def test_que_calculadora_subtrai():
-    calc = Calculadora()
-    result = calc.subtrai(10, 5)
-    assert result == 5
+# def test_que_calculadora_subtrai():
+#     calc = Calculadora()
+#     result = calc.subtrai(10, 5)
+#     assert result == 5
 
-def test_que_calculadora_divisao():
-    calc = Calculadora()
-    result = calc.divisao(6, 2)
-    assert result == 3
-    result = calc.divisao(8, 4)
-    assert result == 2
+# def test_que_calculadora_divisao():
+#     calc = Calculadora()
+#     result = calc.divisao(6, 2)
+#     assert result == 3
+#     result = calc.divisao(8, 4)
+#     assert result == 2
 
 # def test_env_var():
 #     calc = Calculadora()
